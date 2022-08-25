6758f494e1439817aeff735c3a5fa427b2b3ce2b
ztkilla <ztkilla4308@gmail.com>
2022-08-25 15:54:33 +0300

    0

---
+++ script.js
@@ -0,0 +1,6 @@
+function sandwichFunction(x) {
+  document.getElementById('bar1').classList.toggle('active')
+  document.getElementById('bar2').classList.toggle('active')
+  document.getElementById('bar3').classList.toggle('active')
+  document.getElementById('navlinks-container').classList.toggle('active')
+}
